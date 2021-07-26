dbac6843a36deb15f003cd86959a9fb4b4168a56
ewertonfs <ewertonferreirasantiago@gmail.com>
2021-07-26 17:39:08 -0300

    correcoes api

--- src/main/java/com/example/sicapweb/web/controller/UnidadeGestoraController.java
+++ src/main/java/com/example/sicapweb/web/controller/UnidadeGestoraController.java
@@ -1,24 +1,27 @@
 package com.example.sicapweb.web.controller;
 
+import br.gov.to.tce.model.UnidadeGestora;
 import com.example.sicapweb.repository.UnidadeGestoraRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.ModelMap;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/unidadeGestora")
+@RequestMapping({"/unidadeGestora"})
 public class UnidadeGestoraController {
 
     @Autowired
     private UnidadeGestoraRepository unidadeGestoraRepository;
 
-    @GetMapping("/")
-    public String lista(ModelMap model) {
-        model.addAttribute("unidades", unidadeGestoraRepository.findAll());
-        return "unidadeGestora";
+    @CrossOrigin
+    @GetMapping
+    public ResponseEntity<List<UnidadeGestora>> findAll() {
+        List<UnidadeGestora> list = unidadeGestoraRepository.findAll();
+        return ResponseEntity.ok().body(list);
     }
-
-
 }
