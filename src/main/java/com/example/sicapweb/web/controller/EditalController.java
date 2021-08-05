f0199d873542a73e991071405b63fcc3303597c8
klintonsd <klinton.daltio@gmail.com>
2021-08-05 17:23:53 -0300

    Alterações de layout

--- src/main/java/com/example/sicapweb/web/controller/EditalController.java
+++ src/main/java/com/example/sicapweb/web/controller/EditalController.java
@@ -6,10 +6,14 @@ import com.example.sicapweb.repository.EditalRepository;
 import com.example.sicapweb.repository.EmpresaOrganizadoraRepository;
 import com.example.sicapweb.util.PaginacaoUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.math.BigInteger;
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -23,26 +27,67 @@ public class EditalController {
     @Autowired
     private EmpresaOrganizadoraRepository empresaOrganizadoraRepository;
 
-    @GetMapping("/")
-    public String lista(ModelMap model, @RequestParam("page")Optional<Integer> page, @RequestParam("dir")Optional<String> dir, Edital edital) {
-        int paginaAtual = page.orElse(1);
-        String ordem = dir.orElse("asc");
-
-        PaginacaoUtil<Edital> pageEdital = editalRepository.buscaPaginada(paginaAtual, ordem, "numeroEdital");
-
-        model.addAttribute("pageEdital", pageEdital);
-        return "concursoEdital";
+    @CrossOrigin
+    @GetMapping
+    public ResponseEntity<List<Edital>> findAll() {
+        List<Edital> list = editalRepository.findAll();
+        return ResponseEntity.ok().body(list);
     }
 
+    @CrossOrigin
+    @GetMapping(path = {"/{id}"})
+    public ResponseEntity<?> findById(@PathVariable BigInteger id) {
+        Edital list = editalRepository.findById(id);
+        return ResponseEntity.ok().body(list);
+    }
+
+    @CrossOrigin
     @Transactional
-    @PostMapping("salvar")
-    public String salvar(Edital edital) {
+    @PostMapping
+    public ResponseEntity<Edital> create(@RequestBody Edital edital) {
+        edital.setInfoRemessa(editalRepository.buscarPrimeiraRemessa());
         editalRepository.save(edital);
-        return "redirect:/concursoEdital/";
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(edital.getId()).toUri();
+        return ResponseEntity.created(uri).body(edital);
     }
 
-    @ModelAttribute("empresas")
-    public List<EmpresaOrganizadora> empresaOrganizadoraList() {
-        return empresaOrganizadoraRepository.findAll();
+    @CrossOrigin
+    @Transactional
+    @RequestMapping(value = {"/{id}"}, method = RequestMethod.PUT)
+    public ResponseEntity<Edital> update(@RequestBody Edital edital, @PathVariable BigInteger id){
+        edital.setId(id);
+        editalRepository.update(edital);
+        return ResponseEntity.noContent().build();
     }
+
+    @CrossOrigin
+    @Transactional
+    @DeleteMapping
+    public ResponseEntity<?> delete(@PathVariable BigInteger id) {
+        editalRepository.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+//    @GetMapping("/")
+//    public String lista(ModelMap model, @RequestParam("page")Optional<Integer> page, @RequestParam("dir")Optional<String> dir, Edital edital) {
+//        int paginaAtual = page.orElse(1);
+//        String ordem = dir.orElse("asc");
+//
+//        PaginacaoUtil<Edital> pageEdital = editalRepository.buscaPaginada(paginaAtual, ordem, "numeroEdital");
+//
+//        model.addAttribute("pageEdital", pageEdital);
+//        return "concursoEdital";
+//    }
+//
+//    @Transactional
+//    @PostMapping("salvar")
+//    public String salvar(Edital edital) {
+//        editalRepository.save(edital);
+//        return "redirect:/concursoEdital/";
+//    }
+//
+//    @ModelAttribute("empresas")
+//    public List<EmpresaOrganizadora> empresaOrganizadoraList() {
+//        return empresaOrganizadoraRepository.findAll();
+//    }
 }
