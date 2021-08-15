4e4cea6d14fd183c6490ca4107ed4f3e0c30319c
klintonsd <klinton.daltio@gmail.com>
2021-08-15 16:06:36 -0300

    Atualização da tela de edital e criação da tela de edital homologação

--- src/main/java/com/example/sicapweb/web/controller/EditalHomologacaoController.java
+++ src/main/java/com/example/sicapweb/web/controller/EditalHomologacaoController.java
@@ -7,10 +7,14 @@ import com.example.sicapweb.repository.AtoRepository;
 import com.example.sicapweb.repository.EditalHomologacaoRepository;
 import com.example.sicapweb.repository.EditalRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.math.BigInteger;
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -32,13 +36,6 @@ public class EditalHomologacaoController {
         return "concursoHomologacao";
     }
 
-    @Transactional
-    @PostMapping("salvar")
-    public String salvar(EditalHomologacao editalHomologacao) {
-        editalHomologacaoRepository.save(editalHomologacao);
-        return "redirect:/concursoHomologacao/";
-    }
-
     @ModelAttribute("editais")
     public List<Edital> editalList() {
         return editalRepository.findAll();
@@ -47,5 +44,49 @@ public class EditalHomologacaoController {
     @ModelAttribute("atos")
     public List<Ato> atoList() {
         return atoRepository.findAll();
+    }
+
+    @CrossOrigin
+    @GetMapping
+    public ResponseEntity<List<EditalHomologacao>> findAll() {
+        List<EditalHomologacao> list = editalHomologacaoRepository.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @CrossOrigin
+    @GetMapping(path = {"/{id}"})
+    public ResponseEntity<?> findById(@PathVariable BigInteger id) {
+        EditalHomologacao list = editalHomologacaoRepository.findById(id);
+        return ResponseEntity.ok().body(list);
+    }
+
+    @CrossOrigin
+    @Transactional
+    @PostMapping
+    public ResponseEntity<EditalHomologacao> create(@RequestBody EditalHomologacao editalHomologacao) {
+        editalHomologacao.setChave(editalHomologacaoRepository.buscarPrimeiraRemessa());
+        //editalHomologacao.setEmpresaOrganizadora(empresaOrganizadoraRepository.buscaEmpresaPorCnpj(edital.getCnpjEmpresaOrganizadora()));
+        editalHomologacaoRepository.save(editalHomologacao);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(editalHomologacao.getId()).toUri();
+        return ResponseEntity.created(uri).body(editalHomologacao);
+    }
+
+    @CrossOrigin
+    @Transactional
+    @RequestMapping(value = {"/{id}"}, method = RequestMethod.PUT)
+    public ResponseEntity<EditalHomologacao> update(@RequestBody EditalHomologacao editalHomologacao, @PathVariable BigInteger id) {
+        editalHomologacao.setChave(editalRepository.buscarPrimeiraRemessa());
+        editalHomologacao.setId(id);
+        //edital.setEmpresaOrganizadora(empresaOrganizadoraRepository.buscaEmpresaPorCnpj(edital.getCnpjEmpresaOrganizadora()));
+        editalHomologacaoRepository.update(editalHomologacao);
+        return ResponseEntity.noContent().build();
+    }
+
+    @CrossOrigin
+    @Transactional
+    @DeleteMapping(value = {"/{id}"})
+    public ResponseEntity<?> delete(@PathVariable BigInteger id) {
+        editalHomologacaoRepository.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
