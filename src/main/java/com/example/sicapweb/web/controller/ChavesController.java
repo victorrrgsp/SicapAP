5901c9d6d5b62c57ca07d088136780d78302290e
wesleycomp <weslleycomp@gmail.com>
2021-08-19 17:46:55 -0300

    atualizando chaves controller

--- src/main/java/com/example/sicapweb/web/controller/ChavesController.java
+++ src/main/java/com/example/sicapweb/web/controller/ChavesController.java
@@ -7,14 +7,19 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.math.BigInteger;
 import java.net.URI;
+import java.security.MessageDigest;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 @RestController
 @RequestMapping(value="/chaves")
 
 public class ChavesController {
-
     @Autowired
     private ChavesRepository chavesRepository;
 
@@ -29,10 +34,38 @@ public class ChavesController {
     @CrossOrigin
     @Transactional
     @PostMapping("/salvar")
-    public ResponseEntity<AdmAutenticacao> create(@RequestBody  AdmAutenticacao autenticacao) {
-        chavesRepository.save(autenticacao);
+    public ResponseEntity<AdmAutenticacao> create(@RequestBody  AdmAutenticacao autenticacao) throws ParseException {
+
+        Date data = new Date();
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String dataString = df.format(data);
+        Date strToDate = df.parse(dataString);
+
+
+        String value = ""+autenticacao.getExercicio()+"-"+autenticacao.getRemessa()+"-"+autenticacao.getUnidadeGestora().getId()+"-"+dataString+"WRF";
+        String sha1 = "";
+
+        try {
+            //cria a chave hash SHA-1
+            MessageDigest digest = MessageDigest.getInstance("SHA-1");
+            digest.reset();
+            digest.update(value.getBytes("utf8"));
+            sha1 = String.format("%040x", new BigInteger(1, digest.digest()));
+            //System.out.println(sha1);
+            autenticacao.setIdSistema(29);
+            autenticacao.setData(strToDate);
+
+            autenticacao.setChave(sha1);
+            chavesRepository.save(autenticacao);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(autenticacao.getId()).toUri();
         return ResponseEntity.created(uri).body(autenticacao);
+
     }
+
+
 
 }
