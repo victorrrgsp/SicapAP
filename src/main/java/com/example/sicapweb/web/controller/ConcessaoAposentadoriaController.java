1373bf1dab953afb2f1506f12db75c0c18a432e8
Klinton Daltio <klinton.daltio@gmail.com>
2021-08-27 12:11:53 -0300

    Teste inicial para cadastro de arquivos

--- src/main/java/com/example/sicapweb/web/controller/ConcessaoAposentadoriaController.java
+++ src/main/java/com/example/sicapweb/web/controller/ConcessaoAposentadoriaController.java
@@ -4,10 +4,9 @@ import br.gov.to.tce.model.ap.pessoal.Aposentadoria;
 import com.example.sicapweb.repository.AposentadoriaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -23,5 +22,13 @@ public class ConcessaoAposentadoriaController extends DefaultController<Aposenta
     public ResponseEntity<List<Aposentadoria>> findAll() {
         List<Aposentadoria> list = aposentadoriaRepository.findAll();
         return ResponseEntity.ok().body(list);
+    }
+
+    @CrossOrigin
+    @Transactional
+    @PostMapping("/upload")
+    public ResponseEntity<?> addFile(@RequestParam("file") MultipartFile file) {
+
+        return ResponseEntity.ok().body(super.setCastorFile(file, "AposentadoriaArquivos"));
     }
 }
