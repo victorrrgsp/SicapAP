3e93414c5673bc4ddc193b528dc3a85d2e5e976d
Klinton Daltio <klinton.daltio@gmail.com>
2021-09-02 11:57:39 -0300

    Gerenciamento de arquivos de concessórios de Reconducao e Readaptacao

--- src/main/java/com/example/sicapweb/web/controller/ConcessaoReadaptacaoController.java
+++ src/main/java/com/example/sicapweb/web/controller/ConcessaoReadaptacaoController.java
@@ -1,22 +1,27 @@
 package com.example.sicapweb.web.controller;
 
+import br.gov.to.tce.model.ap.concessoes.DocumentoReadaptacao;
 import br.gov.to.tce.model.ap.pessoal.Readaptacao;
-import br.gov.to.tce.model.ap.pessoal.Reintegracao;
+import com.example.sicapweb.repository.DocumentoReadaptacaoRepository;
 import com.example.sicapweb.repository.ReadaptacaoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.ui.ModelMap;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.math.BigInteger;
 import java.util.List;
 
 @RestController
 @RequestMapping("/documentoConcessaoReadaptacao")
-public class ConcessaoReadaptacaoController {
+public class ConcessaoReadaptacaoController extends DefaultController<Readaptacao>{
 
     @Autowired
     private ReadaptacaoRepository readaptacaoRepository;
+
+    @Autowired
+    private DocumentoReadaptacaoRepository documentoReadaptacaoRepository;
 
     @CrossOrigin
     @GetMapping()
@@ -29,6 +34,27 @@ public class ConcessaoReadaptacaoController {
     @GetMapping(path = {"/{id}"})
     public ResponseEntity<?> findById(@PathVariable BigInteger id) {
         Readaptacao list = readaptacaoRepository.findById(id);
+        return ResponseEntity.ok().body(list);
+    }
+
+    @CrossOrigin
+    @Transactional
+    @PostMapping("/upload/{inciso}/{id}")
+    public ResponseEntity<?> addFile(@RequestParam("file") MultipartFile file, @PathVariable String inciso, @PathVariable BigInteger id) {
+        DocumentoReadaptacao documentoReadaptacao = new DocumentoReadaptacao();
+        documentoReadaptacao.setReadaptacao(readaptacaoRepository.findById(id));
+        documentoReadaptacao.setInciso(inciso);
+        String idCastor = super.setCastorFile(file, "Pensao");
+        documentoReadaptacao.setIdCastorFile(idCastor);
+        documentoReadaptacao.setStatus(DocumentoReadaptacao.Status.Informado.getValor());
+        documentoReadaptacaoRepository.save(documentoReadaptacao);
+        return ResponseEntity.ok().body(idCastor);
+    }
+
+    @CrossOrigin
+    @GetMapping(path = {"anexos/{inciso}/{id}"})
+    public ResponseEntity<?> findByDocumento(@PathVariable String inciso, @PathVariable BigInteger id) {
+        DocumentoReadaptacao list = documentoReadaptacaoRepository.buscarDocumentoReadaptacao(inciso, id).get(0);
         return ResponseEntity.ok().body(list);
     }
 }
