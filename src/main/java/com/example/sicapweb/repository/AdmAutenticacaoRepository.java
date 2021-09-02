ce2d284aa6a0cd7b936562f077643771e0a490f3
wesleycomp <weslleycomp@gmail.com>
2021-09-02 16:51:45 -0300

    atualizacao pagiancao 3

--- src/main/java/com/example/sicapweb/repository/AdmAutenticacaoRepository.java
+++ src/main/java/com/example/sicapweb/repository/AdmAutenticacaoRepository.java
@@ -7,6 +7,7 @@ import javax.persistence.EntityManager;
 import java.math.BigInteger;
 import java.util.List;
 
+
 @Repository
 public class AdmAutenticacaoRepository extends DefaultRepository<AdmAutenticacao, BigInteger> {
 
