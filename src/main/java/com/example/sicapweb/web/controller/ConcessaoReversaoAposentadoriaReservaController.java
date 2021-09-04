536bd320995de5d0938651499550e001159ec407
Klinton Daltio <klinton.daltio@gmail.com>
2021-09-04 16:36:33 -0300

    Alteraão dos incisos para o backend e alteração de status após arquivos serem carregados concessórios

--- src/main/java/com/example/sicapweb/web/controller/ConcessaoReversaoAposentadoriaReservaController.java
+++ src/main/java/com/example/sicapweb/web/controller/ConcessaoReversaoAposentadoriaReservaController.java
@@ -2,6 +2,7 @@ package com.example.sicapweb.web.controller;
 
 import br.gov.to.tce.model.ap.concessoes.DocumentoAposentadoria;
 import br.gov.to.tce.model.ap.pessoal.Aposentadoria;
+import com.example.sicapweb.model.Inciso;
 import com.example.sicapweb.repository.AposentadoriaRepository;
 import com.example.sicapweb.repository.DocumentoAposentadoriaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.math.BigInteger;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -44,6 +46,36 @@ public class ConcessaoReversaoAposentadoriaReservaController  extends DefaultCon
         documentoAposentadoria.setReversao("S");
         documentoAposentadoriaRepository.save(documentoAposentadoria);
         return ResponseEntity.ok().body(idCastor);
+    }
+    @CrossOrigin
+    @GetMapping(path = {"getInciso/{id}"})
+    public ResponseEntity<?> findInciso(@PathVariable BigInteger id) {
+        List<Inciso> list = new ArrayList<>();
+        list.add(new Inciso("I - Seção V", "Ofício subscrito pela autoridade competente",
+                "Ofício subscrito pela autoridade competente dirigido ao Presidente do TCE/TO dando ciência do fato", "", "Sim"));
+        list.add(new Inciso("II - Seção V", "Ato da concessão acompanhado da respectiva publicação",
+                "Ato da concessão acompanhado da respectiva publicação", "", "Sim"));
+        list.add(new Inciso("VI - Seção V", "Parecer jurídico atestando a legalidade do ato",
+                "Parecer jurídico atestando a legalidade do ato", "", "Sim"));
+        list.add(new Inciso("IV", "Último contracheque",
+                "Último contracheque", "", "Sim"));
+        list.add(new Inciso("VII - Seção V", "Laudo médico pericial",
+                "Laudo médico pericial declarando a habilitação do aposentado para o exercício do cargo anteriormente ocupado", "", "Sim"));
+        list.add(new Inciso("VIII - Seção V", "Documento de comprovação de idade do servidor",
+                "Documento de comprovação de idade do servidor", "", "Sim"));
+        list.add(new Inciso("", "Outros",
+                "Outros", "", "Não"));
+
+        for (int i = 0; i < list.size(); i++){
+            Integer existeArquivo = documentoAposentadoriaRepository.findAllInciso("documentoAposentadoria","idAposentadoria",id, list.get(i).getInciso());
+            if (existeArquivo > 0){
+                list.get(i).setStatus("Informado");
+            }else{
+                list.get(i).setStatus("Não informado");
+            }
+        }
+
+        return ResponseEntity.ok().body(list);
     }
 
     @CrossOrigin
