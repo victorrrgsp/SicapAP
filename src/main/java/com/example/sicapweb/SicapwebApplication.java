ca4259d96e5355524557ad78706d00ecba5cb3ef
ewertonfs <ewertonferreirasantiago@gmail.com>
2021-09-12 22:37:39 -0300

    commit muito louco feito David "O cara"

--- src/main/java/com/example/sicapweb/SicapwebApplication.java
+++ src/main/java/com/example/sicapweb/SicapwebApplication.java
@@ -6,7 +6,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import redis.clients.jedis.Jedis;
 
 import javax.annotation.PostConstruct;
@@ -82,4 +86,14 @@ public class SicapwebApplication {
 //        }
 //
 //    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:8082");
+            }
+        };
+    }
 }
--- src/main/java/com/example/sicapweb/web/controller/AssinarRemessaController.java
+++ src/main/java/com/example/sicapweb/web/controller/AssinarRemessaController.java
@@ -7,6 +7,7 @@ import okhttp3.RequestBody;
 import okhttp3.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
@@ -41,12 +42,14 @@ public class AssinarRemessaController {
     }
 
     @CrossOrigin
-    @GetMapping(path = {"assinar/{certificado}/{original}"})
-    public ResponseEntity<String> findResponsavel(@PathVariable String certificado, @PathVariable String original) throws IOException {
+    @Transactional
+    @PostMapping(path = {"/inicializarAssinatura"})
+    public ResponseEntity<String> inicializarAssinatura(@org.springframework.web.bind.annotation.RequestBody String postdata) throws IOException {
+
         OkHttpClient client = new OkHttpClient().newBuilder()
                 .build();
         MediaType mediaType = MediaType.parse("application/x-www-form-urlencoded; charset=UTF-8");
-        RequestBody body = RequestBody.create(mediaType, "certificado=" + certificado + "&original=" + original);
+        RequestBody body = RequestBody.create(mediaType, postdata);
         Request request = new Request.Builder()
                 .url("https://dev2.tce.to.gov.br/assinador/app/controllers/?&c=TCE_Assinador_AssinadorWeb&m=inicializarAssinatura")
                 .method("POST", body)
@@ -60,4 +63,49 @@ public class AssinarRemessaController {
 
         return ResponseEntity.ok().body(resposta);
     }
+
+    @CrossOrigin
+    @Transactional
+    @PostMapping(path = {"/assinadorWebfinalizarAssinatura"})
+    public ResponseEntity<String> assinadorWebfinalizarAssinatura(@org.springframework.web.bind.annotation.RequestBody String postdata) throws IOException {
+        OkHttpClient client = new OkHttpClient().newBuilder()
+                .build();
+        MediaType mediaType = MediaType.parse("application/x-www-form-urlencoded; charset=UTF-8");
+        RequestBody body = RequestBody.create(mediaType, postdata);
+        Request request = new Request.Builder()
+                .url("https://dev2.tce.to.gov.br/assinador/app/controllers/?&c=TCE_Assinador_AssinadorWeb&m=finalizarAssinatura")
+                .method("POST", body)
+                .addHeader("Accept", "application/json")
+                .addHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8")
+                .addHeader("Referer", "https://app.tce.to.gov.br/")
+                .build();
+
+        Response response = client.newCall(request).execute();
+        String resposta = response.body().string();
+
+        return ResponseEntity.ok().body(resposta);
+
+    }
+
+    @CrossOrigin
+    @Transactional
+    @PostMapping(path = {"/assinadorSicapApFinalizaAssinatura"})
+    public ResponseEntity<String> assinadorSicapApFinalizaAssinatura(@org.springframework.web.bind.annotation.RequestBody String postdata) throws IOException {
+        OkHttpClient client = new OkHttpClient().newBuilder()
+                .build();
+        MediaType mediaType = MediaType.parse("application/x-www-form-urlencoded; charset=UTF-8");
+        RequestBody body = RequestBody.create(mediaType, postdata + "&cpf="+User.getUser().getCpf()+"&ug="+User.getUser().getUnidadeGestora().getId());
+        Request request = new Request.Builder()
+                .url("https://dev2.tce.to.gov.br/sicapap/app/controllers/?&c=TCE_SicapAp_ApiProcessoEletronico&m=finalizaAssinatura")
+                .method("POST", body)
+                .addHeader("Accept", "application/json")
+                .addHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8")
+                .addHeader("Referer", "https://app.tce.to.gov.br/")
+                .build();
+        Response response = client.newCall(request).execute();
+        String resposta = response.body().string();
+        return ResponseEntity.ok().body(resposta);
+    }
+
 }
+
