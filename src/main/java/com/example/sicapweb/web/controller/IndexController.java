d80ce14577ff48869e8c8443385aa4d60aaed3a0
ewerton Santiago <ewertonferreirasantiago@gmail.com>
2022-02-22 14:29:08 -0300

    testes loucos do Tio Wesley, Eu e JJ

--- src/main/java/com/example/sicapweb/web/controller/IndexController.java
+++ src/main/java/com/example/sicapweb/web/controller/IndexController.java
@@ -4,6 +4,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+
 @RestController
 @RequestMapping("/index")
 public class IndexController {
@@ -11,5 +13,25 @@ public class IndexController {
     @GetMapping("/")
     public String index() {
         return "index";
+    }
+
+    //############################################ API REINICIAR SEVER #####################################################
+    @GetMapping("/script")
+    public void executeCommand() {
+        String[] env = {"PATH=/bin:/usr/local/bin/"};
+
+        String cmd = "reiniciarSicapWeb.sh";  //e.g test.sh -dparam1 -oout.txt
+        //tratamento de erro e execução do script
+
+        try {
+            System.out.println(env);
+
+            Process process = Runtime.getRuntime().exec(cmd, env);
+            System.out.println("tste:"+process);
+
+        } catch (IOException ex) {
+            System.out.println(ex);
+            //  Logger.getLogger(TecMain.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
