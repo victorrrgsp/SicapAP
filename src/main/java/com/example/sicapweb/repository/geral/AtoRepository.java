d1a670e330516e85f0a096c3823209fd7eea97a2
guilhermehse <lopesgui01@gmail.com>
2022-04-18 16:24:04 -0300

    consulta por atos distinct

--- src/main/java/com/example/sicapweb/repository/geral/AtoRepository.java
+++ src/main/java/com/example/sicapweb/repository/geral/AtoRepository.java
@@ -2,14 +2,24 @@ package com.example.sicapweb.repository.geral;
 
 import br.gov.to.tce.model.ap.relacional.Ato;
 import com.example.sicapweb.repository.DefaultRepository;
+import com.example.sicapweb.security.User;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import java.math.BigInteger;
+import java.util.List;
 
 @Repository
 public class AtoRepository extends DefaultRepository<Ato, BigInteger> {
     public AtoRepository(EntityManager em) {
         super(em);
+    }
+
+    public List<Ato> findAll() {
+        return getEntityManager()
+                .createQuery("select distinct a from Ato" +
+                        " a, InfoRemessa info where a.infoRemessa.chave = info.chave and info.idUnidadeGestora = '"
+                        + User.getUser(request).getUnidadeGestora().getId() + "'", Ato.class)
+                .getResultList();
     }
 }
