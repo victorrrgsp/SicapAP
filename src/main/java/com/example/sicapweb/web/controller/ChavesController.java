404bc58159c5421b0aa831d1ef1d54602a8001ea
guilherme01 <lopesgui01@gmail.com>
2022-06-14 14:24:14 -0300

    verificacao au criar chave

--- src/main/java/com/example/sicapweb/web/controller/ChavesController.java
+++ src/main/java/com/example/sicapweb/web/controller/ChavesController.java
@@ -1,5 +1,6 @@
 package com.example.sicapweb.web.controller;
 import br.gov.to.tce.model.adm.AdmAutenticacao;
+import com.example.sicapweb.exception.InvalitInsert;
 import com.example.sicapweb.repository.AdmAutenticacaoRepository;
 import com.example.sicapweb.util.PaginacaoUtil;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -42,10 +43,33 @@ public class ChavesController {
         Date strToDate = df.parse(dataString);
         String value = ""+autenticacao.getExercicio()+"-"+autenticacao.getRemessa()+"-"+autenticacao.getUnidadeGestora().getId()+"-"+dataString+"WRF";
         String sha1 = "";
-
+        Integer remessa;
+        Integer exercicio;
+        if (autenticacao.getRemessa()==1 ) {
+            remessa=12;
+            exercicio= autenticacao.getExercicio()-1;
+        }
+        else{
+            remessa= autenticacao.getRemessa();
+            exercicio= autenticacao.getExercicio();
+        }
+        Integer chavesRemessaAtual =admAutenticacaoRepository.
+                getQtdAssinaturas(
+                        autenticacao.getUnidadeGestora().getId(),
+                        autenticacao.getExercicio(),
+                        autenticacao.getRemessa()
+                );
+        Integer chavesRemessaAnterior =admAutenticacaoRepository.getQtdAssinaturas(autenticacao.getUnidadeGestora().getId(), exercicio, remessa );
+        if (chavesRemessaAtual > 0   ){
+            throw new InvalitInsert("Já existe uma chave com assinatura !!");
+        }
+        if (chavesRemessaAnterior < 3 ||chavesRemessaAnterior  == null) {
+            throw new InvalitInsert("Remessa anterior pendente de assinatura!!");
+        }
         try {
             //cria a chave hash SHA-1
             MessageDigest digest = MessageDigest.getInstance("SHA-1");
+
             digest.reset();
             digest.update(value.getBytes("utf8"));
             sha1 = String.format("%040x", new BigInteger(1, digest.digest()));
