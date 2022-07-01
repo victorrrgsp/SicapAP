a7bcc292e12cc65061121f66e2d6e96266bcfa95
guilherme01 <lopesgui01@gmail.com>
2022-07-01 14:43:34 -0300

    aplicando dto revisao reforma

--- src/main/java/com/example/sicapweb/repository/concessao/AposentadoriaRepository.java
+++ src/main/java/com/example/sicapweb/repository/concessao/AposentadoriaRepository.java
@@ -279,7 +279,7 @@ public class AposentadoriaRepository extends DefaultRepository<Aposentadoria, Bi
     // -------------------------------------------------------------------------------------------------------------- //
     // ----------------------------------------- Concessão Revisão Reforma ------------------------------------------ //
     // -------------------------------------------------------------------------------------------------------------- //
-    public PaginacaoUtil<Aposentadoria> buscaPaginadaRevisaoReforma(Pageable pageable, String searchParams, Integer tipoParams) {
+    public PaginacaoUtil<AposentadoriaDTO> buscaPaginadaRevisaoReforma(Pageable pageable, String searchParams, Integer tipoParams) {
         int pagina = Integer.valueOf(pageable.getPageNumber());
         int tamanho = Integer.valueOf(pageable.getPageSize());
         String search = "";
@@ -287,8 +287,8 @@ public class AposentadoriaRepository extends DefaultRepository<Aposentadoria, Bi
         search = getSearch(searchParams, tipoParams);
         //retirar os : do Sort pageable
         String campo = String.valueOf(pageable.getSort()).replace(":", "");
-
-        List<Aposentadoria> list = getEntityManager()
+        // Object test = new Integer[]{1,2,3,4,5};
+        List<Object[]> list = getEntityManager()
                 .createNativeQuery("select a.cpfServidor, ser.nome, car.nomeCargo, a.tipoAposentadoria, ato.numeroAto, " +
                         " (CASE WHEN ae.status IS NULL THEN 1 ELSE ae.status END) as status, a.id " +
                                 " from Aposentadoria a " +
@@ -299,13 +299,18 @@ public class AposentadoriaRepository extends DefaultRepository<Aposentadoria, Bi
                                 " left join AdmEnvio ae on ae.idMovimentacao = a.id " +
                                 " join InfoRemessa i on a.chave = i.chave " +
                         " where a.reversao = 0 and a.revisao = 1 and a.tipoAposentadoria = " + Aposentadoria.TipoAposentadoria.Reforma.getValor() +
-                        " and i.idUnidadeGestora = '" + User.getUser(super.request).getUnidadeGestora().getId() + "' " + search + " ORDER BY " + campo, Aposentadoria.class)
+                        " and i.idUnidadeGestora = '" + User.getUser(super.request).getUnidadeGestora().getId() + "' " + search + " ORDER BY " + campo)
                 .setFirstResult(pagina)
                 .setMaxResults(tamanho)
                 .getResultList();
+
+        Iterator result = list.iterator();
+        List<AposentadoriaDTO> aposentadoriaDTOList = new ArrayList<>();
+        convertResult(result, aposentadoriaDTOList);
+
         long totalRegistros = countAposentadoriaRevisaoReforma();
         long totalPaginas = (totalRegistros + (tamanho - 1)) / tamanho;
-        return new PaginacaoUtil<Aposentadoria>(tamanho, pagina, totalPaginas, totalRegistros, list);
+        return new PaginacaoUtil<AposentadoriaDTO>(tamanho, pagina, totalPaginas, totalRegistros,aposentadoriaDTOList);
     }
 
     public Integer countAposentadoriaRevisaoReforma() {
