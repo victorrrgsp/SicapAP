07768a0ea9969123acf3e8ccff44b17b8c4bc2e6
Ewerton Santiago <ewertonferreirasantiago@gmail.com>
2022-07-06 15:35:24 -0300

    tela de assinatura do gestor

--- src/main/java/com/example/sicapweb/web/controller/ap/concessao/AdmEnvioController.java
+++ src/main/java/com/example/sicapweb/web/controller/ap/concessao/AdmEnvioController.java
@@ -2,10 +2,8 @@ package com.example.sicapweb.web.controller.ap.concessao;
 
 import br.gov.to.tce.model.adm.AdmEnvio;
 import com.example.sicapweb.repository.AdmEnvioRepository;
-import com.example.sicapweb.util.PaginacaoUtil;
 import com.example.sicapweb.web.controller.DefaultController;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,7 +12,7 @@ import java.util.HashMap;
 @CrossOrigin
 @RestController
 @RequestMapping("/admEnvio")
-public class admEnvioController extends DefaultController<AdmEnvio> {
+public class AdmEnvioController extends DefaultController<AdmEnvio> {
 
     @Autowired
     private AdmEnvioRepository admEnvioRepository;
