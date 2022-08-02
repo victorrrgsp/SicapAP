9573aeb92e3398aa2fd425ddbe49be8cd1a2212f
Ewerton Santiago <ewertonferreirasantiago@gmail.com>
2022-08-02 18:50:00 -0300

    resolvendo problema do sicap ap externo

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import store from '@/store'
 
 export const api = axios.create({
-   baseURL: `${process.env.VUE_APP_HTTP_API}`,
+   baseURL: `https://www.tceto.tc.br/backend`,
     headers: {
         'Content-type': 'application/x-www-form-urlencoded', 
         'Accept': 'application/json'
