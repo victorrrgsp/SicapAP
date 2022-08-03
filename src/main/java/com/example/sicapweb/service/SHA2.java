47b8c9ac18405923d09daf573d879949d3900146
Ewerton Santiago <ewertonferreirasantiago@gmail.com>
2022-08-03 17:48:55 -0300

    resolvendo problema do sicap ap externo

--- src/main/java/com/example/sicapweb/service/SHA2.java
+++ src/main/java/com/example/sicapweb/service/SHA2.java
@@ -34,4 +34,15 @@ public class SHA2 {
 
         return toReturn;
     }
+
+    public static String stringHexa(byte[] bytes) {
+        StringBuilder s = new StringBuilder();
+        for (int i = 0; i < bytes.length; i++) {
+            int parteAlta = ((bytes[i] >> 4) & 0xf) << 4;
+            int parteBaixa = bytes[i] & 0xf;
+            if (parteAlta == 0) s.append('0');
+            s.append(Integer.toHexString(parteAlta | parteBaixa));
+        }
+        return s.toString();
+    }
 }
