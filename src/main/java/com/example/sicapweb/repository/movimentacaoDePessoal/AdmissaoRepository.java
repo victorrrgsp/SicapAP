f7c2d28fe940c87b321f16dbf453e77ffc062371
josermc <joserobertoccomp@gmail.com>
2022-08-19 13:15:07 -0300

    melhoria na tela de concessaoAdmissao admissao repository

--- src/main/java/com/example/sicapweb/repository/movimentacaoDePessoal/AdmissaoRepository.java
+++ src/main/java/com/example/sicapweb/repository/movimentacaoDePessoal/AdmissaoRepository.java
@@ -13,115 +13,135 @@ import javax.persistence.EntityManager;
 import javax.persistence.Query;
 import java.math.BigInteger;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Repository
-    public class AdmissaoRepository extends DefaultRepository<Admissao, BigInteger> {
+public class AdmissaoRepository extends DefaultRepository<Admissao, BigInteger> {
 
-        public AdmissaoRepository(EntityManager em) {
-            super(em);
-        }
+    public AdmissaoRepository(EntityManager em) {
+        super(em);
+    }
 
-        public String getSearch(String searchParams, Integer tipoParams) {
-            String search = "";
-            //monta pesquisa search
-            if (searchParams.length() > 3) {
+    public String getSearch(String searchParams, Integer tipoParams) {
+        String search = "";
+        //monta pesquisa search
+        if (searchParams.length() > 3) {
+            List<String> lparam =  new ArrayList(Arrays.asList(searchParams.split("&")));
+            for (String param : lparam){
                 if (tipoParams == 0) { //entra para tratar a string
-                    String arrayOfStrings[] = searchParams.split("=");
-                    if (arrayOfStrings[0].equals("numeroEdital"))
-                        search = " and a." + arrayOfStrings[0] + " LIKE '%" + arrayOfStrings[1] + "%'  ";
-                    else if (arrayOfStrings[0].equals("veiculoPublicacao"))
-                        search = " and a." + arrayOfStrings[0] + " LIKE '%" + arrayOfStrings[1] + "%'  ";
-                    else if (arrayOfStrings[0].equals("dataPublicacao"))
-                        search = " and a." + arrayOfStrings[0] + " LIKE '%" + arrayOfStrings[1] + "%'  ";
-                    else
-                        search = " and " + arrayOfStrings[0] + " LIKE '%" + arrayOfStrings[1] + "%'  ";
+                    String arrayOfStrings[] = param.split("=");
+                    if (arrayOfStrings[0].equals("cpfServidor")){
+                        search = search + " AND " + arrayOfStrings[0]  + " = '" + arrayOfStrings[1] + "'  ";
+                    } else if (arrayOfStrings[0].equals("idAto")) {
+                        search = search + " AND " + arrayOfStrings[0]  + " = " + arrayOfStrings[1] + "  ";
+                    }
+
                 } else {
-                    search = " and " + searchParams + "   ";
+                    search = " AND " + searchParams + "   ";
                 }
             }
-            return search;
         }
+//            if (searchParams.length() > 3) {
+//                if (tipoParams == 0) { //entra para tratar a string
+//                    String arrayOfStrings[] = searchParams.split("=");
+//                    if (arrayOfStrings[0].equals("cpfServidor"))
+//                        search = " and s." + arrayOfStrings[0] + " = '" + arrayOfStrings[1] + "'  ";
+//                    else if (arrayOfStrings[0].equals("idedital1"))
+//
+//                       search="";
+//                    else if (arrayOfStrings[0].equals("dataPublicacao"))
+//                        search="";
+//                    else
+//                        search = " and " + arrayOfStrings[0] + " LIKE '%" + arrayOfStrings[1] + "%'  ";
+//                } else {
+//                    search = " and " + searchParams + "   ";
+//                }
+//            }
+        return search;
+    }
 
-        public PaginacaoUtil<NomeacaoConcurso> buscarAdmissoes(Pageable pageable, String searchParams, Integer tipoParams) {
-            int pagina = Integer.valueOf(pageable.getPageNumber());
-            int tamanho = Integer.valueOf(pageable.getPageSize());
-            String search = "";
-            search = getSearch(searchParams, tipoParams);
-            String campo = String.valueOf(pageable.getSort()).replace(":", "");
+    public PaginacaoUtil<NomeacaoConcurso> buscarAdmissoes(Pageable pageable, String searchParams, Integer tipoParams) {
+        int pagina = Integer.valueOf(pageable.getPageNumber());
+        int tamanho = Integer.valueOf(pageable.getPageSize());
+        String search = "";
+        search = getSearch(searchParams, tipoParams);
+        String campo = String.valueOf(pageable.getSort()).replace(":", "");
 
-            List<Admissao> list = getEntityManager()
-                    .createNativeQuery("select a.* from Admissao a " +
-                            "join InfoRemessa i on a.chave = i.chave and i.idUnidadeGestora = :ug " +
-                            "where a.tipoAdmissao =1 " + " " + search + " ORDER BY " + campo, Admissao.class)
+        List<Admissao> list = getEntityManager()
+                .createNativeQuery("with admissao1  as " +
+                        "( select a.* from Admissao a  join InfoRemessa i on a.chave = i.chave and i.idUnidadeGestora = :ug and a.tipoAdmissao=1  ), " +
+                        " servidor1 as ( select d.* from Servidor d  join InfoRemessa i on d.chave = i.chave and i.idUnidadeGestora = :ug  ) " +
+                        "select c.* from admissao1 c  join  servidor  s on   c.idServidor = s.id " + " " + search + " ORDER BY " + campo, Admissao.class)
+                .setParameter("ug",User.getUser(super.request).getUnidadeGestora().getId())
+                .setFirstResult(pagina)
+                .setMaxResults(tamanho)
+                .getResultList();
+
+
+        long totalRegistros = countAdmissoes(search);
+        long totalPaginas = (totalRegistros + (tamanho - 1)) / tamanho;
+        List<NomeacaoConcurso> listc= new ArrayList<NomeacaoConcurso>() ;
+        for(Integer i= 0; i < list.size(); i++){
+            NomeacaoConcurso nc =new NomeacaoConcurso();
+            nc.setNome(list.get(i).getServidor().getNome());
+            nc.setCpf(list.get(i).getServidor().getCpfServidor());
+            nc.setAto(list.get(i).getAto());
+            nc.setNumeroEdital(list.get(i).getNumeroEdital());
+            nc.setAdmissao(list.get(i));
+            List<EditalAprovado> ea =  getEntityManager()
+                    .createNativeQuery(" with vaga as ( " +
+                            "   select v.* from  EditalVaga v join InfoRemessa i  on v.chave=i.chave and i.idUnidadeGestora = :ug  and v.idCargo = :idcargo" +
+                            "), " +
+                            "Aprovado as (" +
+                            "   select v.* from  EditalAprovado v join InfoRemessa i  on v.chave=i.chave and i.idUnidadeGestora = :ug and cpf= :cpf" +
+                            " ) " +
+                            "select a.* from EditalAprovado a " +
+                            "join EditalVaga b on a.idEditalVaga= b.id "  +
+                            " where   b.idCargo = :idcargo and   cpf= :cpf ", EditalAprovado.class)
                     .setParameter("ug",User.getUser(super.request).getUnidadeGestora().getId())
-                    .setFirstResult(pagina)
-                    .setMaxResults(tamanho)
+                    .setParameter("idcargo",list.get(i).getCargo().getId())
+                    .setParameter("cpf",nc.getCpf())
                     .getResultList();
 
 
-            long totalRegistros = countAdmissoes();
-            long totalPaginas = (totalRegistros + (tamanho - 1)) / tamanho;
-            List<NomeacaoConcurso> listc= new ArrayList<NomeacaoConcurso>() ;
-            for(Integer i= 0; i < list.size(); i++){
-                NomeacaoConcurso nc =new NomeacaoConcurso();
-                nc.setNome(list.get(i).getServidor().getNome());
-                nc.setCpf(list.get(i).getServidor().getCpfServidor());
-                nc.setAto(list.get(i).getAto());
-                nc.setNumeroEdital(list.get(i).getNumeroEdital());
-                nc.setAdmissao(list.get(i));
-                List<EditalAprovado> ea =  getEntityManager()
-                        .createNativeQuery(" with vaga as ( " +
-                                "   select v.* from  EditalVaga v join InfoRemessa i  on v.chave=i.chave and i.idUnidadeGestora = :ug " +
-                                "), " +
-                                "Aprovado as (" +
-                                "   select v.* from  EditalAprovado v join InfoRemessa i  on v.chave=i.chave and i.idUnidadeGestora = :ug " +
-                                " ) " +
-                                "select a.* from EditalAprovado a " +
-                                "join EditalVaga b on a.idEditalVaga= b.id "  +
-                                " where  b.idCargo = :idcargo and   cpf= :cpf ", EditalAprovado.class)
-                        .setParameter("ug",User.getUser(super.request).getUnidadeGestora().getId())
-                        .setParameter("idcargo",list.get(i).getCargo().getId())
-                        .setParameter("cpf",nc.getCpf())
-                        .getResultList();
 
+            if (ea.size()>0 ){
+                nc.setEditalAprovado((EditalAprovado) ea.get(0) );
 
+                nc.setClassificacao(ea.get(0).getClassificacao());
+                nc.setSitCadAprovado("Aprovado Cadastrado");
+                nc.setVaga(ea.get(0).getEditalVaga().getCargo().getCargoNome().getNome());
+                Integer enviado = (Integer) getEntityManager().createNativeQuery("select count(*) from DocumentoAdmissao a " +
+                        "where  a.idAdmissao = "+list.get(i).getId()+ "").getSingleResult();
+                if (enviado > 0 ){
 
-                if (ea.size()>0 ){
-                    nc.setEditalAprovado((EditalAprovado) ea.get(0) );
-
-                    nc.setClassificacao(ea.get(0).getClassificacao());
-                    nc.setSitCadAprovado("Aprovado Cadastrado");
-                    nc.setVaga(ea.get(0).getEditalVaga().getCargo().getCargoNome().getNome());
-                    Integer enviado = (Integer) getEntityManager().createNativeQuery("select count(*) from DocumentoAdmissao a " +
-                            "where  a.idAdmissao = "+list.get(i).getId()+ "").getSingleResult();
-                    if (enviado > 0 ){
-
-                        nc.setSituacaoNomeacao("Aprovado anexado");
-                    }
-                    else
-                    {
-                        nc.setSituacaoNomeacao("Apto para envio!");
-                    }
+                    nc.setSituacaoNomeacao("Aprovado anexado");
                 }
                 else
                 {
-                    nc.setSitCadAprovado("Aprovado não Cadastrado!");
-                    nc.setSituacaoNomeacao("inapto para envio!");
+                    nc.setSituacaoNomeacao("Apto para envio!");
                 }
-
-                listc.add(nc);
             }
-            return new PaginacaoUtil<NomeacaoConcurso>(tamanho, pagina, totalPaginas, totalRegistros, listc);
-        }
+            else
+            {
+                nc.setSitCadAprovado("Aprovado não Cadastrado!");
+                nc.setSituacaoNomeacao("inapto para envio!");
+            }
 
-        public Integer countAdmissoes() {
-            Query query = getEntityManager().createNativeQuery("select count(*) from Admissao a " +
-                    "join InfoRemessa i on a.chave = i.chave and i.idUnidadeGestora = :ug " +
-                    "where  a.tipoAdmissao =1 ")
-                    .setParameter("ug",User.getUser(super.request).getUnidadeGestora().getId())
-                    ;
-            return (Integer) query.getSingleResult();
+            listc.add(nc);
         }
-
+        return new PaginacaoUtil<NomeacaoConcurso>(tamanho, pagina, totalPaginas, totalRegistros, listc);
     }
+
+    public Integer countAdmissoes(String search) {
+        Query query = getEntityManager().createNativeQuery("with admissao1  as " +
+                        "( select a.* from Admissao a  join InfoRemessa i on a.chave = i.chave and i.idUnidadeGestora = :ug and a.tipoAdmissao=1  ), " +
+                        " servidor1 as ( select d.* from Servidor d  join InfoRemessa i on d.chave = i.chave and i.idUnidadeGestora = :ug  ) " +
+                        "select count(1) from admissao1 c  join  servidor  s on   c.idServidor = s.id " + " " + search)
+                .setParameter("ug",User.getUser(super.request).getUnidadeGestora().getId())
+                ;
+        return (Integer) query.getSingleResult();
+    }
+
+}
