4d68be538c9626d262f46770eff984b1bfaa20f2
guilherme01 <lopesgui01@gmail.com>
2022-08-31 16:37:07 -0300

    alterando consulta por UDs

--- src/main/java/com/example/sicapweb/repository/geral/UnidadeAdministrativaRepository.java
+++ src/main/java/com/example/sicapweb/repository/geral/UnidadeAdministrativaRepository.java
@@ -65,16 +65,23 @@ public class UnidadeAdministrativaRepository extends DefaultRepository<UnidadeAd
 
     public List<HashMap<String, Object>> pesquisaPorUg(String ug) {
         List<Object[]> list = getEntityManager().createNativeQuery(
-                "select distinct\n" +
+                "with uds as(select distinct\n" +
+                        "\n" +
                         "    ug.cnpj as ugCnpj,\n" +
                         "    ud.cnpj as udCnpj,\n" +
+                        "    ud.id,\n" +
+                        "    ud.codigoUnidadeAdministrativa ,\n" +
                         "    ug.descricao as UnidadeGestora ,\n" +
-                        "    ud.nome as unidadeAdministrativa\n" +
+                        "    ud.nome as unidadeAdministrativa,\n" +
+                        "    RANK() OVER (PARTITION BY ud.codigoUnidadeAdministrativa ORDER BY ud.id desc ) as rank\n" +
+                        "\n" +
                         "from SicapAP21.dbo.LOTACAO l\n" +
                         "    join UnidadeAdministrativa  ud on l.idUnidadeAdministrativa = ud.id\n" +
-                        "    join InfoRemessa info on l.chave = info.chave\n" +
+                        "\tjoin InfoRemessa info on l.chave = info.chave\n" +
                         "    join SicapAP.dbo.unidadegestora Ug on info.idUnidadeGestora = ug.cnpj\n" +
-                        "where (ug.cnpj = :UnidadeGestora or :UnidadeGestora = 'todos' )")
+                        "where (ug.cnpj = :UnidadeGestora or :UnidadeGestora = 'todos' )\n" +
+                        ")\n" +
+                        "select * from uds where uds.rank = 1")
                 .setParameter("UnidadeGestora", ug)
                 .getResultList();
         List<HashMap<String, Object>> retorno = new ArrayList<HashMap<String, Object>>();
@@ -82,10 +89,12 @@ public class UnidadeAdministrativaRepository extends DefaultRepository<UnidadeAd
         list.forEach(envio -> {
             var aux = new HashMap<String, Object>();
             // aux.put("id", envio[0] );
-            aux.put("ugCnpj", envio[0]);
-            aux.put("udCnpj", envio[1]);
-            aux.put("UnidadeGestora", envio[2]);
-            aux.put("unidadeAdministrativa", envio[3]);
+            aux.put("ugCnpj"               , envio[0]);
+            aux.put("udCnpj"               , envio[1]);
+            aux.put("udId"                 , envio[2]);
+            aux.put("codigoUnidadeAdministrativa"                 , envio[3]);
+            aux.put("UnidadeGestora"       , envio[4]);
+            aux.put("unidadeAdministrativa", envio[5]);
 
             retorno.add(aux);
         });
