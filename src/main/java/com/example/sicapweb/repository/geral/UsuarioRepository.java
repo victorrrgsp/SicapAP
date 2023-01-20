a7835d54fbdba1a90edcd71177399f56df285e6e
Ewerton Santiago <ewertonferreirasantiago@gmail.com>
2023-01-20 14:59:47 -0300

    correcao liberacao dos usuarios automaticos a partir do cadun

--- src/main/java/com/example/sicapweb/repository/geral/UsuarioRepository.java
+++ src/main/java/com/example/sicapweb/repository/geral/UsuarioRepository.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import java.math.BigInteger;
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
@@ -16,11 +17,34 @@ public class UsuarioRepository extends DefaultRepository<Cargo, BigInteger> {
     }
 
     public List<Object> getUser(String codigo, Integer sistema) {
-        return getEntityManager().createNativeQuery("   " +
-                "select cpf, unidadeGestora, nomeEntidade, codpoder, Cargo  " +
-                "from AutenticacaoAssinatura..usuario a  " +
-                "join AutenticacaoAssinatura..UsuarioAplicacao b on a.CPF = b.Usuario " +
-                "join UnidadeGestoraCadun c on UnidadeGestora = cnpj " +
-                "where cpf=  '"+codigo+"' and cargo in (4,5,3,32,362) and Aplicacao = "+ sistema).getResultList();
+        var autorizados = List.of("02908453193", "55786553191", "02925533159");
+
+        if (autorizados.contains(codigo)) {
+            return getEntityManager().createNativeQuery("   " +
+                    "select cpf, unidadeGestora, nomeEntidade, codpoder, Cargo  " +
+                    "from AutenticacaoAssinatura..usuario a  " +
+                    "join AutenticacaoAssinatura..UsuarioAplicacao b on a.CPF = b.Usuario " +
+                    "join UnidadeGestoraCadun c on UnidadeGestora = cnpj " +
+                    "where cpf=  '" + codigo + "' and cargo in (4,5,3,32,362) and Aplicacao = " + sistema).getResultList();
+        }
+
+        return getEntityManager().createNativeQuery(
+                "select distinct pf.cpf, upc.cnpj, upc.nomeEntidade, uni.poder, upc.idCargo " +
+                        "      from cadun.dbo.vwUnidadesPessoasCargos upc " +
+                        "               join cadun.dbo.vwpessoa pf on pf.id = upc.idPessoaFisica " +
+                        "               join Cadun.dbo.PessoaJuridica pj on upc.idPessoaJuridica = pj.Codigo " +
+                        "               join SICAPAP21..UnidadeGestora uni on uni.id = upc.cnpj " +
+                        "      where upc.idCargo in (3, 4, 32, 362) " +
+                        "        and upc.ativo = 1 " +
+                        "        and pf.cpf = '" + codigo + "'").getResultList();
+
+
+    }
+
+    public Integer buscarCargoPorCpf(String cpf) {
+        return (Integer) getEntityManager().createNativeQuery(
+                "select idCargo " +
+                        "from Cadun..vwUnidadesPessoasCargos " +
+                        "where cpf = '"+ cpf +"';").getSingleResult();
     }
 }
