b0344cc42437cd955fd182ab4a28dfd937fc22d1
Ewerton Santiago <ewertonferreirasantiago@gmail.com>
2023-01-31 17:29:17 -0300

    tentando corrigir exclusão

--- src/main/java/com/example/sicapweb/SicapwebApplication.java
+++ src/main/java/com/example/sicapweb/SicapwebApplication.java
@@ -47,16 +47,4 @@ public class SicapwebApplication {
             }
         };
     }
-
-    @Bean
-    CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("*"));
-        configuration.setAllowedMethods(Arrays.asList("*"));
-        configuration.setAllowedHeaders(Arrays.asList("*"));
-        configuration.setAllowCredentials(true);
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
-    }
 }
