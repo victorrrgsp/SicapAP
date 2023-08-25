210bcf5099f10df5a275abc780cf3252934264fa
guilhermehse <lopesgui01@gmail.com>
2023-08-25 08:15:46 -0300

    definindo endPoint de pesquisa

--- src/main/java/com/example/sicapweb/web/controller/ap/geral/RelatoriosController.java
+++ src/main/java/com/example/sicapweb/web/controller/ap/geral/RelatoriosController.java
@@ -35,18 +35,57 @@ public class RelatoriosController extends DefaultController<Lei> {
                                                                     @RequestParam(required = false) String folhaItem,
                                                                     @RequestParam(required = false) String cargo,
                                                                     @RequestParam String UnidadeGestora){
-        List<HashMap<String, Object>> result = relatorioRepository.buscarfolhaAnalitica(cpf, 
-                                                                                        nome, 
-                                                                                        Natureza, 
+        List<HashMap<String, Object>> result = relatorioRepository.buscarfolhaAnalitica(cpf,
+                                                                                        nome,
+                                                                                        Natureza,
                                                                                         Vinculo,
-                                                                                        ano, 
-                                                                                        mes, 
-                                                                                        lotacao, 
-                                                                                        UnidadeAdministrativa, 
+                                                                                        ano,
+                                                                                        mes,
+                                                                                        lotacao,
+                                                                                        UnidadeAdministrativa,
                                                                                         UnidadeGestora,
                                                                                         folhaItem,
                                                                                         cargo);
         return ResponseEntity.ok().body(result);
     }
+    @CrossOrigin
+    @GetMapping(path = "/servidoresEmFolha")
+    public ResponseEntity<List<HashMap<String, Object>>> listServidoresFolha(@RequestParam(required = false) String cpf,
+                                                                    @RequestParam(required = false) String nome,
+                                                                    @RequestParam(required = false) String Natureza,
+                                                                    @RequestParam(required = false) List<String> Vinculo,
+                                                                    @RequestParam int ano,
+                                                                    @RequestParam int mes,
+                                                                    @RequestParam(required = false) List<String> lotacao,
+                                                                    @RequestParam(required = false) List<String> UnidadeAdministrativa,
+                                                                    @RequestParam(required = false) String folhaItem,
+                                                                    @RequestParam(required = false) String cargo,
+                                                                    @RequestParam String UnidadeGestora){
+        List<HashMap<String, Object>> result = relatorioRepository.buscarPesoasfolha(cpf,
+                                                                                        nome,
+                                                                                        Natureza,
+                                                                                        Vinculo,
+                                                                                        ano,
+                                                                                        mes,
+                                                                                        lotacao,
+                                                                                        UnidadeAdministrativa,
+                                                                                        UnidadeGestora,
+                                                                                        folhaItem,
+                                                                                        cargo);
+        return ResponseEntity.ok().body(result);
+    }
+    @CrossOrigin
+    @GetMapping(path = "/folhaServidor")
+    public ResponseEntity<List<HashMap<String, Object>>> listfolhaServidor(
+                                                                    @RequestParam String matriculaServidor,
+                                                                    @RequestParam(required = false) String Natureza,
+                                                                    @RequestParam int ano,
+                                                                    @RequestParam int mes,
+                                                                    @RequestParam(required = false) String folhaItem
+                                                                    ){
+        List<HashMap<String, Object>> result = relatorioRepository.buscarFolhaPesoas(matriculaServidor, Natureza, ano, mes, folhaItem);
+        return ResponseEntity.ok().body(result);
+    }
 
 }
+
