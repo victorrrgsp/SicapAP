1b4e91b6bb2f4336d79ad25fc344fd720a2ac9d1
guilhermehse <lopesgui01@gmail.com>
2023-08-25 08:15:05 -0300

    definindo metodo de pesquisa

--- src/main/java/com/example/sicapweb/repository/geral/RelatorioRepository.java
+++ src/main/java/com/example/sicapweb/repository/geral/RelatorioRepository.java
@@ -80,18 +80,144 @@ public class RelatorioRepository extends DefaultRepository<Lei, BigInteger> {
                                 .setParameter("cargo", cargo)
                                 .setParameter("UnidadeGestora", UnidadeGestora);
                                 
-                                if(UnidadeAdministrativa != null){
-                                    query.setParameter("UnidadeAdministrativa", UnidadeAdministrativa);
-                                }if(Natureza != null){
-                                    query.setParameter("Natureza", Natureza);
-                                }if(folhaItem != null){
-                                    query.setParameter("folhaItem", folhaItem);
-                                }if(Vinculo != null){
-                                    query.setParameter("Vinculo", Vinculo);
-                                }if(lotacao != null){
-                                    query.setParameter("lotacao", lotacao);
-                                }
+        if(UnidadeAdministrativa != null){
+            query.setParameter("UnidadeAdministrativa", UnidadeAdministrativa);
+        }if(Natureza != null){
+            query.setParameter("Natureza", Natureza);
+        }if(folhaItem != null){
+            query.setParameter("folhaItem", folhaItem);
+        }if(Vinculo != null){
+            query.setParameter("Vinculo", Vinculo);
+        }if(lotacao != null){
+            query.setParameter("lotacao", lotacao);
+        }
 
-                                return StaticMethods.getHashmapFromQuery(query);
+        return StaticMethods.getHashmapFromQuery(query);
+    }
+
+    public List<HashMap<String, Object>> buscarFolhaPesoas( String matriculaServidor , String Natureza, int ano, int mes, String folhaItem ) {
+        var query = getEntityManager().createNativeQuery(
+                    "with principal as(\n" +
+                            "    select distinct NaturezaRubrica , codigoFolhaItem,\n" +
+                            "       wfp.TipoFolha                                                 as FolhaTipo,\n" +
+                            "       wfp.classe                                                    as regime,\n" +
+                            "       wfp.folhaItemUnidadeGestora                                   as FolhaItem,\n" +
+                            "       wfp.natureza                                          as FolhaEsocial,\n" +
+                            "       wfp.matriculaServidor as matriculaServidor,\n" +
+                            "       wfp.CodigoFolhaItemESocial,\n" +
+                            "       (case wfp.NaturezaRubrica when 'Vantagem' then wfp.valor end) as Vantagem,\n" +
+                            "       (case wfp.NaturezaRubrica when 'Desconto' then wfp.valor end) as Descontos,\n" +
+                            "       (case wfp.NaturezaRubrica\n" +
+                            "            when 'Vantagem' then wfp.valor\n" +
+                            "            when 'Desconto' then (wfp.valor * -1) end)  as Valor\n" +
+                            "from vwFolhaPagamento wfp\n" +
+                            "join SICAPAP21.dbo.Lotacao l on wfp.idLotacao = l.id\n" +
+                            "join UnidadeAdministrativa ud on l.idUnidadeAdministrativa = ud.id\n" +
+                            "where (:UnidadeGestora = 'todos' or wfp.idUnidadeGestora = :UnidadeGestora )\n" +
+                            "\n" +
+                            "  and wfp.exercicio = :Ano\n" +
+                            "  and (wfp.remessa = :Mes or :Mes = null )\n" +
+                            (Natureza != null?"  and wfp.NaturezaRubrica in :Natureza\n":"") +
+                            "  and wfp.folhaItemUnidadeGestora not like 'Base%'\n" +
+                            (folhaItem != null?"  and wfp.FolhaItemUnidadeGestora like '%'+ :folhaItem +'%'\n":"") +
+                            ")\n" +
+                            "select distinct * from principal\n" +
+                            "order by NaturezaRubrica desc, codigoFolhaItem;")
+                            .setParameter("matriculaServidor", matriculaServidor );
+                            
+        if(Natureza != null){
+            query.setParameter("Natureza", Natureza);
+        }if(folhaItem != null){
+            query.setParameter("folhaItem", folhaItem);
+        }
+        return StaticMethods.getHashmapFromQuery(query);
+    }
+
+
+
+    public List<HashMap<String, Object>> buscarPesoasfolha( String cpf, String nome, String Natureza, List<String> Vinculo, int ano, int mes, List<String> lotacao, List<String> UnidadeAdministrativa, String UnidadeGestora, String folhaItem,String cargo) {
+        var query = getEntityManager().createNativeQuery(
+                        "with principal as(\n" +
+                                "    select distinct ud.codigoUnidadeAdministrativa,\n" +
+                                "       wfp.idUnidadeGestora                                         as CNPJ,\n" +
+                                "       wfp.unidadeGestora                                            as Entidade,\n" +
+                                "       YEAR(wfp.Competencia)                                         as Ano,\n" +
+                                "       MONTH(wfp.Competencia)                                        as Mes,\n" +
+                                "       wfp.nome                                                      as Nome,\n" +
+                                "       wfp.cpfServidor                                               as CPF,\n" +
+                                "       wfp.cargo                                                     as Cargo,\n" +
+                                "       wfp.nomeCargoOrigem                                           as cargoUg,\n" +
+                                "       wfp.TipoAdmissao                                              as FolhaVinculo,\n" +
+                                "       wfp.JornadaFolha                                              as jornada,\n" +
+                                "       wfp.matriculaServidor as matriculaServidor,\n" +
+                                "       wfp.TipoAto,\n" +
+                                "       wfp.NumeroAtoAdmissao,\n" +
+                                "       wfp.nomeLotacao,\n" +
+                                "       ud.nome as unidadeAdministrativa ,\n" +
+                                "       sum((case wfp.NaturezaRubrica when 'Vantagem' then wfp.valor end)) as Vantagem,\n" +
+                                "       sum((case wfp.NaturezaRubrica when 'Desconto' then wfp.valor end)) as Descontos,\n" +
+                                "       sum((case wfp.NaturezaRubrica\n" +
+                                "            when 'Vantagem' then wfp.valor\n" +
+                                "            when 'Desconto' then (wfp.valor * -1) end)) as Liquido\n" +
+                                "from vwFolhaPagamento wfp\n" +
+                                "join SICAPAP21.dbo.Lotacao l on wfp.idLotacao = l.id\n" +
+                                "join UnidadeAdministrativa ud on l.idUnidadeAdministrativa = ud.id\n" +
+                                "where (:UnidadeGestora = 'todos' or wfp.idUnidadeGestora = :UnidadeGestora )\n" +
+                                (UnidadeAdministrativa != null?"  and ud.codigoUnidadeAdministrativa  in :UnidadeAdministrativa\n":"") +
+                                "\n" +
+                                "  and wfp.exercicio = :Ano\n" +
+                                "  and (wfp.remessa = :Mes or :Mes = null )\n" +
+                                (lotacao != null?"  and wfp.nomeLotacao in :lotacao\n":"") +
+                                (Vinculo != null?"  and wfp.TipoAdmissao in :Vinculo\n":"") +
+                                (Natureza != null?"  and wfp.NaturezaRubrica in :Natureza\n":"") +
+                                "  and wfp.folhaItemUnidadeGestora not like 'Base%'\n" +
+                                "  and (:nome is null or upper(wfp.nome) like'%'+upper(:nome)+'%')\n" +
+                                "  and (\n" +
+                                "      (:cpf is null or\n" +
+                                "            (:cargo is null or\n" +
+                                "             wfp.nomeCargoOrigem = :cargo)\n" +
+                                "            )\n" +
+                                "    or wfp.cpfServidor like concat('%', :cpf, '%'))\n" +
+                                (folhaItem != null?"  and wfp.FolhaItemUnidadeGestora like '%'+ :folhaItem +'%'\n":"") +
+                                "group by\n" +
+                                "    ud.codigoUnidadeAdministrativa,\n" +
+                                "    wfp.idUnidadeGestora                                            ,\n" +
+                                "    wfp.unidadeGestora                                              ,\n" +
+                                "    wfp.Competencia                                                 ,\n" +
+                                "    wfp.nome                                                        ,\n" +
+                                "    wfp.cpfServidor                                                 ,\n" +
+                                "    wfp.cargo                                                       ,\n" +
+                                "    wfp.nomeCargoOrigem                                             ,\n" +
+                                "    wfp.TipoAdmissao                                                ,\n" +
+                                "    wfp.JornadaFolha                                                ,\n" +
+                                "    wfp.matriculaServidor                                           ,\n" +
+                                "    wfp.TipoAto                                                     ,\n" +
+                                "    wfp.NumeroAtoAdmissao                                           ,\n" +
+                                "    wfp.nomeLotacao                                                 ,\n" +
+                                "    ud.nome\n" +
+                                ")\n" +
+                                "\n" +
+                                "select distinct * from principal\n" +
+                                "order by nome;")
+                                .setParameter("cpf", cpf )
+                                .setParameter("nome", nome)
+                                .setParameter("Ano", ano)
+                                .setParameter("Mes", mes)
+                                .setParameter("cargo", cargo)
+                                .setParameter("UnidadeGestora", UnidadeGestora);
+                                
+        if(UnidadeAdministrativa != null){
+            query.setParameter("UnidadeAdministrativa", UnidadeAdministrativa);
+        }if(Natureza != null){
+            query.setParameter("Natureza", Natureza);
+        }if(folhaItem != null){
+            query.setParameter("folhaItem", folhaItem);
+        }if(Vinculo != null){
+            query.setParameter("Vinculo", Vinculo);
+        }if(lotacao != null){
+            query.setParameter("lotacao", lotacao);
+        }
+
+        return StaticMethods.getHashmapFromQuery(query);
     }
 }
